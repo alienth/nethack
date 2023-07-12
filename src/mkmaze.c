alienth/nethack

commit ff727e916f58cd18fb8e2c1d57e4d416134234a1

work around a build issue with a compiler
One compiler was issuing an error diagnostic for cmap_to_glyph() macro (see below).
This just works around that by using cmap_a_to_glyph() which does not suffer the same fate.

mkmaze.c

    static const struct rm water_pos = { cmap_to_glyph(S_water), WATER, 0, 0,
.........................................^
%CC-E-NEEDCONSTEXPR, In the initializer for water_pos.glyph, "In_mines(...)" is not constant, but occurs in a context that requires a constant expression.
at line number 1448 in file mkmaze.c

    static const struct rm water_pos = { cmap_to_glyph(S_water), WATER, 0, 0,
.........................................^
%CC-E-NEEDCONSTEXPR, In the initializer for water_pos.glyph, "In_hell(...)" is not constant, but occurs in a context that requires a constant expression.
at line number 1448 in file mkmaze.c

    static const struct rm water_pos = { cmap_to_glyph(S_water), WATER, 0, 0,
.........................................^
%CC-E-NEEDCONSTEXPR, In the initializer for water_pos.glyph, "gd" is not constant, but occurs in a context that requires a constant expression.
at line number 1448 in file mkmaze.c

    static const struct rm water_pos = { cmap_to_glyph(S_water), WATER, 0, 0,
.........................................^
%CC-E-NEEDCONSTEXPR, In the initializer for water_pos.glyph, "on_level(...)" is not constant, but occurs in a context that requires a constant expression.
at line number 1448 in file mkmaze.c

    static const struct rm water_pos = { cmap_to_glyph(S_water), WATER, 0, 0,
.........................................^
%CC-E-NEEDCONSTEXPR, In the initializer for water_pos.glyph, "(&u.uz)->dnum" is not constant, but occurs in a context that requires a constant expression.
at line number 1448 in file mkmaze.c

    static const struct rm water_pos = { cmap_to_glyph(S_water), WATER, 0, 0,
.........................................^

src/mkmaze.c
@@ -1445,9 +1445,9 @@ static void mv_bubble(struct bubble *, coordxy, coordxy, boolean);
 void
 movebubbles(void)
 {
-    static const struct rm water_pos = { cmap_to_glyph(S_water), WATER, 0, 0,
+    static const struct rm water_pos = { cmap_a_to_glyph(S_water), WATER, 0, 0,
                                          0, 0, 0, 0, 0, 0 };
-    static const struct rm air_pos = { cmap_to_glyph(S_cloud), AIR, 0, 0, 0,
+    static const struct rm air_pos = { cmap_a_to_glyph(S_cloud), AIR, 0, 0, 0,
                                        1, 0, 0, 0, 0 };
     static boolean up = FALSE;
     struct bubble *b;

commit 28d70dc86cf57994b5eb079887eac1b3346e2f0b

Fix: wrong glyphs for clouds and water on Planes
ff727e9 introduced an issue where unseen water and cloud on the
respective planes were shown with open and closed drawbridge glyphs
instead of the appropriate glyphs.  This is because they fall into cmap
section B, but the translation from symbols/cmap index to glyphs was
being done as though they were in cmap section A (ff727e9 manually used
that particular cmap section macro instead of the general cmap_to_glyph
to work around some compiler warning).

src/mkmaze.c
@@ -1465,9 +1465,9 @@ static void mv_bubble(struct bubble *, coordxy, coordxy, boolean);
 void
 movebubbles(void)
 {
-    static const struct rm water_pos = { cmap_a_to_glyph(S_water), WATER, 0, 0,
-                                         0, 0, 0, 0, 0, 0 };
-    static const struct rm air_pos = { cmap_a_to_glyph(S_cloud), AIR, 0, 0, 0,
+    static const struct rm water_pos = { cmap_b_to_glyph(S_water), WATER, 0,
+                                         0, 0, 0, 0, 0, 0, 0 };
+    static const struct rm air_pos = { cmap_b_to_glyph(S_cloud), AIR, 0, 0, 0,
                                        1, 0, 0, 0, 0 };
     static boolean up = FALSE;
     struct bubble *b;